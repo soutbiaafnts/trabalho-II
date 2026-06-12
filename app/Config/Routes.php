1376854407af8e2022c8887d79ae81c0912c4bf1
soutbiaafnts <bianca.fontes.dev@gmail.com>
2026-06-11 23:52:31 -0300

    :sparkles: feat: initial list view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,5 @@ $routes->get('/', 'Home::index');
 $routes->get('/municipios/estado/(:num)', 'Municipios::getByEstado/$1');
 $routes->post('/cadastro', 'Cadastro::cadastro');
 
+// CRUD Clientes
+$routes->get('/clientes', 'Cliente::index');
